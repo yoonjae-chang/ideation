70648744f47cb11193fc3938a0d81b9e4ea729a0
Yoon Jae Chang <yc4579@columbia.edu>
2025-09-12 12:11:28 -0400

    fix: motion div

--- client/src/components/magicui/animated-list.tsx
+++ client/src/components/magicui/animated-list.tsx
@@ -10,15 +10,15 @@ import React, {
 } from "react";
 
 export function AnimatedListItem({ children }: { children: React.ReactNode }) {
-  const animations = {
-    initial: { scale: 0, opacity: 0 },
-    animate: { scale: 1, opacity: 1, originY: 0 },
-    exit: { scale: 0, opacity: 0 },
-    transition: { type: "spring", stiffness: 350, damping: 40 },
-  };
-
   return (
-    <motion.div {...animations} layout className="mx-auto w-full">
+    <motion.div 
+      initial={{ scale: 0, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1, originY: 0 }}
+      exit={{ scale: 0, opacity: 0 }}
+      transition={{ type: "spring" as const, stiffness: 350, damping: 40 }}
+      layout 
+      className="mx-auto w-full"
+    >
       {children}
     </motion.div>
   );
