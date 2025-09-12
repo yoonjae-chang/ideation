4a8a7c87fa6f5ca3179062ba16ff10b3aab29917
Yoon Jae Chang <yc4579@columbia.edu>
2025-09-12 04:24:21 -0400

    fix: authenticate ideating page

--- client/src/lib/supabase/middleware.ts
+++ client/src/lib/supabase/middleware.ts
@@ -51,8 +51,7 @@ export async function updateSession(request: NextRequest) {
     request.nextUrl.pathname !== "/" &&
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth") &&
-    !request.nextUrl.pathname.startsWith("/ideating")
+    !request.nextUrl.pathname.startsWith("/auth")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
